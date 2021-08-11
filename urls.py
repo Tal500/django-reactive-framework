963250891c8756f1082603bdb76d657002d4afd7
Tal Hadad <tal_hd@hotmail.com>
2021-08-11 12:53:31 +0300

    Fixing url paths

--- urls.py
+++ urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from django.views.generic import TemplateView
 
 urlpatterns = [
-    path('/example/',
+    path('example/',
         TemplateView.as_view(template_name='reactive_example.html', extra_context={'example_list': ['Apple', 'Orange', 'Banana']})),
 ]
