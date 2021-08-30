ac0ced5b918d07b0f1998cfdaeba6d1b6c07c572
Tal Hadad <tal_hd@hotmail.com>
2021-08-30 21:52:16 +0300

    Add strict inequallity operator.

--- core/reactive_binary_operators.py
+++ core/reactive_binary_operators.py
@@ -41,4 +41,28 @@ class StrictEqualityOperator(ReactiveBinaryOperator):
 
 ReactiveBinaryOperator.operators['==='] = StrictEqualityOperator()
 
+class StrictInequalityOperator(ReactiveBinaryOperator):
+    def eval_initial(self, reactive_context: ReactContext, lhs: 'Expression', rhs: 'Expression') -> ReactValType:
+        self.validate_args(lhs, rhs)
+        
+        lhs_val = lhs.eval_initial(reactive_context)
+        rhs_val = rhs.eval_initial(reactive_context)
+
+        result = lhs_val is not rhs_val
+
+        if result and isinstance(lhs_val, str) and isinstance(rhs_val, str):
+            result = (lhs_val != rhs_val)
+        
+        return result
+
+    def eval_js(self, reactive_context: ReactContext, delimiter: str, lhs: 'Expression', rhs: 'Expression') -> str:
+        self.validate_args(lhs, rhs)
+        
+        lhs_val = lhs.eval_js_and_hooks(reactive_context)[0]
+        rhs_val = rhs.eval_js_and_hooks(reactive_context)[0]
+
+        return f'({lhs_val}!=={rhs_val})'
+
+ReactiveBinaryOperator.operators['!=='] = StrictInequalityOperator()
+
 from .expressions import *
