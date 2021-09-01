369a2ec45bcc8cef04e59c74fe0e07ebce801242
Tal Hadad <tal_hd@hotmail.com>
2021-09-01 14:41:06 +0300

    Add CustomReactiveFunction

--- core/reactive_function.py
+++ core/reactive_function.py
@@ -19,6 +19,23 @@ class ReactiveFunction:
     def eval_js(self, reactive_context: ReactContext, delimiter: str, args: List['Expression']) -> str:
         pass
 
+class CustomReactiveFunction(ReactiveFunction):
+    def __init__(self, eval_initial_func, eval_js_func, validate_args_func = lambda args: True):
+        self.validate_args_func = validate_args_func
+        self.eval_initial_func = eval_initial_func
+        self.eval_js_func = eval_js_func
+
+        super().__init__()
+
+    def validate_args(self, args: List['Expression']) -> None:
+        self.validate_args_func(args)
+
+    def eval_initial(self, reactive_context: ReactContext, args: List['Expression']) -> ReactValType:
+        return self.eval_initial_func(reactive_context, args)
+    
+    def eval_js(self, reactive_context: ReactContext, delimiter: str, args: List['Expression']) -> str:
+        return self.eval_js_func(reactive_context, delimiter, args)
+
 class LengthFunction(ReactiveFunction):
     def validate_args(self, args: List['Expression']) -> None:
         if (len(args) != 1):
