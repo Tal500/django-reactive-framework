a4b325099861667b483040d1c2bea2f52b4fc940
Tal Hadad <tal_hd@hotmail.com>
2021-09-13 12:35:23 +0300

    Add present function, with an example (due to #6)

--- src/django_reactive_framework/core/reactive_function.py
+++ src/django_reactive_framework/core/reactive_function.py
@@ -44,6 +44,33 @@ class CustomReactiveFunction(ReactiveFunction):
     def eval_js(self, reactive_context: ReactContext, delimiter: str, args: List['Expression']) -> str:
         return self.eval_js_func(reactive_context, delimiter, args)
 
+class PresentFunction(ReactiveFunction):
+    def validate_args(self, args: List['Expression']) -> None:
+        if (len(args) != 1):
+            raise template.TemplateSyntaxError(f'Error while evaluating present function: ' + \
+                f'there must be exactly one argument! args: ({args})')
+
+    def eval_initial(self, reactive_context: ReactContext, args: List['Expression']) -> ReactValType:
+        self.validate_args(args)
+        
+        val = args[0].eval_initial(reactive_context)
+
+        return val
+
+    def eval_js(self, reactive_context: ReactContext, delimiter: str, args: List['Expression']) -> str:
+        self.validate_args(args)
+        
+        js, hooks = args[0].eval_js_and_hooks(reactive_context)
+
+        return js, []
+
+    def eval_hooks(self, reactive_context: ReactContext, args: List['Expression']) -> str:
+        self.validate_args(args)
+
+        return []
+
+ReactiveFunction.functions['present'] = PresentFunction()
+
 class LengthFunction(ReactiveFunction):
     def validate_args(self, args: List['Expression']) -> None:
         if (len(args) != 1):
